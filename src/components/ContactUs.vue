5bc258e66c37701301b13704047c6030f1ce241e
JuanI <juanpalaciagar@gmail.com>
2023-08-03 12:36:04 +0200

    Small update to the contact page to open a modal when a submit is successfully validated.

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -7,7 +7,14 @@
                     <p>Do you have any helpful tip or constructive criticism to make this page better than it is? Send it our way!</p>
                 </header>
                 <main>
-                        <v-form fast-fail v-model="valid">
+                  <v-overlay z-index="1000" v-model="overlay" class="overlay">
+                      <div class="fill-height overlay-success-container d-flex">
+
+                      <h3 class="mb-4">Comment submitted successfully!</h3>
+                      <v-btn @click="overlay = false">Close</v-btn>
+                  </div>
+                </v-overlay>
+                        <v-form fast-fail v-model="valid" @submit.prevent="valid ? overlay = true : null">
                           <v-container>
                             <v-row>
                               <v-col
@@ -67,6 +74,7 @@
     data: () => ({
       valid: false,
       username: '',
+      overlay: false,
       nameRules: [
         value => {
           if (value) return true
@@ -104,3 +112,100 @@
     }),
   }
 </script>
+
+<style scoped>
+
+
+.overlay-success-container{
+padding: 3rem;
+ translate: -0.1px 40vh;
+  border: 5px groove teal;
+  background-color: #121212;
+  color: whitesmoke;
+  flex-direction:column;
+
+}
+
+.overlay-success-container > button{
+text-align:center;
+margin: auto;
+
+}
+
+@media (min-width:26rem){
+  .overlay-success-container{
+    translate: 1rem 40vh;
+  }
+}
+
+@media (min-width: 31rem){
+  .overlay-success-container{
+    translate: 11.5vw 40vh;
+  }
+}
+
+@media (min-width: 37rem){
+  .overlay-success-container{
+
+    padding: 4rem;
+    translate: 14vw 40vh;
+  }
+}
+
+@media (min-width: 43rem){
+  .overlay-success-container{
+
+    padding: 4rem;
+    translate: 18.5vw 40vh;
+  }
+}
+
+@media (min-width: 50rem){
+  .overlay-success-container{
+
+    padding: 5rem 4rem;
+    translate: 25vw 35vh;
+  }
+}
+
+@media (min-width: 56rem){
+  .overlay-success-container{
+
+    padding: 5rem 4rem;
+    translate: 27vw 35vh;
+  }
+}
+
+@media (min-width: 62rem){
+  .overlay-success-container{
+
+    padding: 5rem 4rem;
+    translate: 28vw 35vh;
+  }
+}
+
+@media (min-width: 68rem){
+  .overlay-success-container{
+
+    padding: 5rem 4rem;
+    translate: 31vw 35vh;
+  }
+}
+
+@media (min-width: 75rem){
+  .overlay-success-container{
+
+    padding: 5rem 4rem;
+    translate: 33vw 35vh;
+  }
+}
+
+@media (min-width: 81rem){
+  .overlay-success-container{
+
+    padding: 5rem 4rem;
+    translate: 42vw 35vh;
+  }
+}
+
+</style>
